94ab40b0a4bba4f82d70e02204be5502a0b49cf0
exoticchild <therealenny1@gmail.com>
2021-01-19 17:17:15 -0600

    Avocado Pi

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<!-- just a placeholder -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <style>
+            body {
+                background-color: lime;
+            }
+        </style>
+        <meta http-equiv="refresh" content="1; URL=index.html" />
+      <div style="text-align:center;"><div class="spinner-border text-success" style="margin-top:300px;width:100px;height:100px;"></div></div>
+      <p style="text-align:center;font-size:30px;">Loading...</p>
+    </body>
+</html>
